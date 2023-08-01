6fd53562d6aeb03c81efb7e91c77ceaee31376c2
pepo1120 <pepogameryt@gmail.com>
2023-07-31 20:41:57 -0500

    Creacion de la entidad products

---
+++ src/main/java/com/microservice/serviceproducts/ServiceProductsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.serviceproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProductsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/microservice/serviceproducts/models/entity/Producto.java
@@ -0,0 +1,66 @@
+package com.microservice.serviceproducts.models.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private BigDecimal precio;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreateAt() {
+        return this.createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    private static final long serialVersionUID = 9646270634L;
+
+
+}
